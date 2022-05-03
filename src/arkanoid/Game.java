0341c9a9d2c91a59b2fe2a24fd7164e1e7627bb3
Pau4Serra <2001pauserra@gmail.com>
2022-05-03 17:34:53 +0200

    First commit

---
+++ src/arkanoid/Game.java
@@ -0,0 +1,46 @@
+package arkanoid;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.geom.Ellipse2D;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+
+public class Game extends JPanel {
+	
+	Ball ball = new Ball(this);
+	
+	private void move() {
+		ball.move();
+	}
+	
+	@Override
+	public void paint(Graphics g) {
+		super.paint(g);
+		Graphics2D g2d = (Graphics2D) g;
+		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+				RenderingHints.VALUE_ANTIALIAS_ON);
+		ball.paint(g2d);
+		
+	}
+
+	public static void main(String[] args) throws InterruptedException {
+
+		JFrame frame = new JFrame("Arkanoid");
+		Game game = new Game();
+		frame.add(game);
+		frame.setSize(300, 400);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		while (true) {
+			game.move();
+			game.repaint();
+			Thread.sleep(8);
+		}
+	}
+
+}
